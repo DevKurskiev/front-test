d89630da7003e17fdf2bd2814f4d70d635a53a87
DevKurskiev <devkurskiev@gmail.com>
2025-02-27 23:18:25 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './context/AuthContext';
-import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
+    <div>Adam</div>
   </React.StrictMode>
 );
