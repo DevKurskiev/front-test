bc3f7d0592fa2fb16935fc00dfbaebe51e52146f
DevKurskiev <devkurskiev@gmail.com>
2025-03-01 03:54:25 +0300

    r

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:3000",
+      target: "http://77.223.107.162:3000",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
     })
